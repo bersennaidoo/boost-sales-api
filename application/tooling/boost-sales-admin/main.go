0934f6869083fe7a4ef66c661890a33bfe17cb4c
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-10-16 12:03:07 +0200

    implemented logic for create private public key for jwt and validating token

---
+++ application/tooling/boost-sales-admin/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func main() {
+	_ = genToken()
+
+}
+
+func genToken() error {
+
+	name := "infrastructure/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem"
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	claims := struct {
+		jwt.StandardClaims
+		Roles []string
+	}{
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "mserve project",
+			Subject:   "123456789",
+			ExpiresAt: time.Now().Add(8760 * time.Hour).Unix(),
+			IssuedAt:  time.Now().UTC().Unix(),
+		},
+		Roles: []string{"BOOST ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	tokenStr, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("======== TOKEN BEGIN ========")
+	fmt.Println(tokenStr)
+	fmt.Println("======== TOKEN END ========")
+	fmt.Print("\n")
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("=========================")
+
+	parser := jwt.Parser{
+		ValidMethods: []string{"RS256"},
+	}
+
+	keyFunc := func(t *jwt.Token) (interface{}, error) {
+		kid, ok := t.Header["kid"]
+		if !ok {
+			return nil, errors.New("missing key id (kid) in token header")
+		}
+		kidID, ok := kid.(string)
+		if !ok {
+			return nil, errors.New("user token key id (kid) must be string")
+		}
+
+		fmt.Println("KID:", kidID)
+		return &privateKey.PublicKey, nil
+	}
+
+	var parsedClaims struct {
+		jwt.StandardClaims
+		Roles []string
+	}
+	parsedToken, err := parser.ParseWithClaims(tokenStr, &parsedClaims, keyFunc)
+	if err != nil {
+		return fmt.Errorf("parsing token: %w", err)
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	fmt.Println("Token Validated")
+
+	return nil
+}
+
+func genKey() error {
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		return fmt.Errorf("creating private file: %w", err)
+	}
+	defer privateFile.Close()
+
+	privateBlock := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	if err := pem.Encode(privateFile, &privateBlock); err != nil {
+		return fmt.Errorf("encoding to private file: %w", err)
+	}
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		return fmt.Errorf("creating public file: %w", err)
+	}
+	defer publicFile.Close()
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("private and public key files generated")
+	return nil
+}
